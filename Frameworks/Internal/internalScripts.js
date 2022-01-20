ead9277b967caf9b8d9daaaed17132b3743540e0
Nathan White <nwhitemisc@gmail.com>
2022-01-19 19:25:12 -0500

    Update to bootstrap 5.1.3

--- Frameworks/Internal/internalScripts.js
+++ Frameworks/Internal/internalScripts.js
@@ -4,14 +4,20 @@ window.addEventListener("afterprint", onPageCompletePrint);
 var readdNoprint = false;
 
 function onPagePrint() {
+    //set date on print page
     var datetime = new Date().toLocaleString();
     var dateText = document.getElementById("timestamp");
     dateText.innerText = "Printed: ".concat(datetime);
+
+    //Show comment section if user selects it.
     var commentArea = document.getElementById("commentSection");
     var includeCommentArea = document.getElementById("includeCommentSection").checked;
     if (includeCommentArea && commentArea != null) {
         readdNoprint = true;
-        commentArea.classList.toggle("d-print-none");
+        commentArea.classList.toggle("d-none");
+        var textEntry = document.getElementById("commentSectionText").value;
+        var commentText = convertTextareaToHTML(textEntry);
+        document.getElementById("commentContent").innerHTML = commentText;
     }
 };
 
@@ -19,7 +25,7 @@ function onPageCompletePrint() {
     var commentArea = document.getElementById("commentSection");
     if (readdNoprint) {
         readdNoprint = false;
-        commentArea.classList.toggle("d-print-none");
+        commentArea.classList.toggle("d-none");
     }
 }
 
@@ -35,4 +41,15 @@ function uncheckAll() {
     }
     var commentArea = document.getElementById("commentSectionText");
     commentArea.value = "";
+}
+
+function convertTextareaToHTML(textArea) {
+    var input = textArea;
+    console.log(input);
+    var lines = input.split( '\n' );
+    var html = '';
+    for( var i=0; i<lines.length; i++ ) {
+        html += '<p class="my-1">' + lines[i] + '</p>';
+    }
+    return html;
 }
