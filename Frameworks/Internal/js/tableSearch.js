64884e5c74e323f8d3ba5f79780fd629d4b67c37
Nathan White <nwhitemisc@gmail.com>
2022-04-08 08:47:08 -0400

    Code List update

--- Frameworks/Internal/js/tableSearch.js
+++ Frameworks/Internal/js/tableSearch.js
@@ -1,14 +1,17 @@
-function searchTable(text, id) {
+function searchTable() {
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, tr, td, i, txtValue, column;
   input = document.getElementById("searchBar");
+  column = getRadioGroupValue("btnradio");
   filter = input.value.toUpperCase();
   table = document.getElementById("tableCodes");
   tr = table.getElementsByTagName("tr");
 
+  var columnId = (column.toString() == "btnradio1") ? 0 : 2;
+
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[columnId];
     if (td) {
       txtValue =  td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -18,4 +21,10 @@ function searchTable(text, id) {
       }
     }
   }
+}
+
+function clearSearch() {
+  var input = document.getElementById("searchBar");
+  input.value = "";
+  searchTable();
 }
